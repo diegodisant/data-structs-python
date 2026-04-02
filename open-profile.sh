ba7a48a46f5046d0d57683ce46cdea754785c0b6
diegodisant <developerdiego0@gmail.com>
2026-04-01 20:08:54 -0600

    refactor: open profile directly without including dir

--- open-profile.sh
+++ open-profile.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if [ "$#" -eq 0 ]; then
-  echo 'Error: a test_name wasnt provided please use as follows'
-  echo "${0} test_name"
+  echo "Error: a test_name wasn't not provided"
+  echo "Usage: ${0} test_name"
 
   exit 1
 fi
 
-python3 -m snakeviz $1
+python3 -m snakeviz profiling/$1.prof
 
 exit 0
